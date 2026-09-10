47f0010bf3e85fc95b1cda07d894ff9274405d42
SiDesmundo <git.kilometer106@passmail.com>
2026-09-10 16:42:11 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -5,7 +5,7 @@ cd /d "%~dp0"
 if exist ".venv\Scripts\python.exe" (set "PY=.venv\Scripts\python.exe") else (set "PY=python")
 
 echo.
-echo   scraperpro
+echo   scraperpro by loredans
 echo   ----------
 echo    1  Princeton Tec
 echo    2  Crispi
